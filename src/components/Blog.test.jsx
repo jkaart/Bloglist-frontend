c858e699c29f3673ff39596acbc7e5c1fea9ecea
Jani Kaartinen <jkaart@gmail.com>
2025-08-18 11:19:52 +0300

    Eslint: styles fixed

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -24,7 +24,7 @@ test('renders blog title', () => {
 describe('user click event', () => {
   let blog
   let user
-  
+
   beforeEach(() => {
     blog = {
       title: 'Go To Statement Considered Harmful',
